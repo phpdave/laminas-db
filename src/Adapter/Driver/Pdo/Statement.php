c0dc403fcbb2d55c1c14517654b976c57d620c41
PHPDave <davelhs03@yahoo.com>
2024-05-27 22:17:16 -0400

    Added modifications to PDO Statement.php for handling output parameters with DB2 for I

--- src/Adapter/Driver/Pdo/Statement.php
+++ src/Adapter/Driver/Pdo/Statement.php
@@ -42,7 +42,20 @@ class Statement implements StatementInterface, Profiler\ProfilerAwareInterface
 
     /** @var bool */
     protected $isPrepared = false;
-
+    
+    // ALAN
+    // @var array
+    protected $colLengths = [];
+    
+    // ALAN
+    // @var array
+    protected $colTypes = [];
+    
+    // DAVE
+    // @var string
+    protected $sprocName = [];
+    
+    
     /**
      * Set driver
      *
@@ -155,7 +168,48 @@ class Statement implements StatementInterface, Profiler\ProfilerAwareInterface
         if ($sql === null) {
             $sql = $this->sql;
         }
+        // begin AS
+        // get params
+        $isSproc = preg_match('\'CALL (.*)[\\(]\'', $sql, $matches);
+        if ($isSproc) {
+            $sprocName = strtoupper($matches[1]);
+            $library = '';
+            if(strpos(strtolower($_ENV['application_environment']), 'test') !== false || 
+               strpos(strtolower($_ENV['application_environment']), 'development') !== false){
+                $library = $_ENV['library_development'];
+            } else if(strpos(strtolower($_ENV['application_environment']), 'prod') !== false){
+                $library = $_ENV['library_production'];
+            }
+            $colSql = "select ORDINAL_POSITION, CHARACTER_MAXIMUM_LENGTH, DATA_TYPE, NUMERIC_PRECISION, NUMERIC_SCALE "
+                    . "from sysparms where SPECIFIC_NAME = '$sprocName' and SPECIFIC_SCHEMA = '$library'";
+            $colStmt = $this->pdo->prepare($colSql);
+            $colResult = $colStmt->execute();
+            $rows = $colStmt->fetchAll();
+            $this->colLengths = [];
+            $this->colTypes = [];
+            $this->sprocName = $sprocName;
+            $numeric_datatype_array = array('SMALLINT','BIGINT','DOUBLE PRECISION','DECIMAL','REAL','NUMERIC','INTEGER');
 
+            foreach ($rows as $row) {
+                $position = $row['ORDINAL_POSITION'];
+                $exportRow = var_export($row,true);
+
+                if(in_array($row['DATA_TYPE'],$numeric_datatype_array)) {
+
+                    $this->colLengths[$position] = $row['NUMERIC_PRECISION'];
+                    if(!is_null($row['NUMERIC_SCALE']) && $row['NUMERIC_SCALE'] > 0) {
+                        $this->colLengths[$position]++;
+                    }
+                } else {
+                    $length = $row['CHARACTER_MAXIMUM_LENGTH'];
+                    $this->colLengths[$position] = $length;
+                }
+                $type = $row['DATA_TYPE'];
+                $this->colTypes[$position] = $type;
+            }
+        }
+        // end AS
+        
         $this->resource = $this->pdo->prepare($sql);
 
         if ($this->resource === false) {
@@ -179,8 +233,13 @@ class Statement implements StatementInterface, Profiler\ProfilerAwareInterface
      * @throws Exception\InvalidQueryException
      * @return Result
      */
-    public function execute($parameters = null)
+    public function execute($parameters = null, $directions = null)
     {
+        
+        
+        // which constants indicate output.
+        $outputConstantValues = array(DB2_PARAM_OUT, DB2_PARAM_INOUT);
+        
         if (! $this->isPrepared) {
             $this->prepare();
         }
@@ -200,76 +259,97 @@ class Statement implements StatementInterface, Profiler\ProfilerAwareInterface
         }
 
         if ($this->parameterContainer->count() > 0) {
-            $this->bindParametersFromContainer();
+            if (!$this->parametersBound) {
+                $parameters = $this->parameterContainer->getNamedArray();
+                $directionsKeys = array_keys($directions);
+                $directionsValues = array_values($directions);
+                $idx=0;
+                foreach ($parameters as $name => &$value) {
+                    if (is_bool($value)) {
+                        $type = \PDO::PARAM_BOOL;
+                    } elseif (is_int($value)) {
+                        $type = \PDO::PARAM_INT;
+                    } else {
+                        $type = \PDO::PARAM_STR;
+                    }
+                    if ($this->parameterContainer->offsetHasErrata($name)) {
+                        switch ($this->parameterContainer->offsetGetErrata($name)) {
+                            case ParameterContainer::TYPE_INTEGER:
+                                $type = \PDO::PARAM_INT;
+                                break;
+                            case ParameterContainer::TYPE_NULL:
+                                $type = \PDO::PARAM_NULL;
+                                break;
+                            case ParameterContainer::TYPE_LOB:
+                                $type = \PDO::PARAM_LOB;
+                                break;
+                        }
+                    }
+                    
+                    $paramPosition = $idx+1; // 1-based index. AS made this
+                    //If a directions array was passed in then bind output parameters
+                    if ($directions){
+                        $maxLength = $this->colLengths[$paramPosition] + 1; // for ODBC
+                        $colType = $this->colTypes[$paramPosition]; // for ODBC
+                        if(in_array($directionsValues[$idx], $outputConstantValues)){ // compat with ibm_db2 caller even if constant not defined.
+                            if ($colType == 'CHARACTER' or $colType == 'CHARACTER VARYING') {
+                                $this->resource->bindParam($paramPosition, $value, $type|\PDO::PARAM_INPUT_OUTPUT, $maxLength);
+                            } else if ($this->sprocName == $_ENV['stored_proc_name_for_out_param_check1'] && $paramPosition==5 ) {
+                                $this->resource->bindParam($paramPosition, $value, $type|\PDO::PARAM_INPUT_OUTPUT, 9);
+                            } else if ($this->sprocName == $_ENV['stored_proc_name_for_out_param_check2'] && $paramPosition==4 ) {
+                                $this->resource->bindParam($paramPosition, $value, $type|\PDO::PARAM_INPUT_OUTPUT, 9);
+                            } else {
+                                $this->resource->bindParam($paramPosition, $value, $type|\PDO::PARAM_INPUT_OUTPUT);
+                            }
+                            
+                        }
+                        //It's an input parameter
+                        else {
+                            
+                            $this->resource->bindParam($paramPosition, $value, $type);
+                            
+                        }
+                    }
+                    else{
+                        $this->resource->bindParam($paramPosition, $value, $type);
+                    }
+                    $idx++;
+                } //(foreach)
+            }
         }
         /** END Standard ParameterContainer Merging Block */
-
+        
         if ($this->profiler) {
             $this->profiler->profilerStart($this);
         }
-
+        
         try {
             $this->resource->execute();
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             if ($this->profiler) {
                 $this->profiler->profilerFinish();
             }
-
-            $code = $e->getCode();
-            if (! is_int($code)) {
-                $code = 0;
-            }
-
             throw new Exception\InvalidQueryException(
                 'Statement could not be executed (' . implode(' - ', $this->resource->errorInfo()) . ')',
-                $code,
+                null,
                 $e
-            );
+                );
         }
-
+        
+        if (is_array($parameters) || is_object($parameters))
+        {
+            foreach ($parameters as $name => &$value) {
+                $this->parameterContainer->offsetSet ( $name, $value);
+            }
+        }
+        
         if ($this->profiler) {
             $this->profiler->profilerFinish();
         }
-
-        return $this->driver->createResult($this->resource, $this);
-    }
-
-    /**
-     * Bind parameters from container
-     */
-    protected function bindParametersFromContainer()
-    {
-        if ($this->parametersBound) {
-            return;
-        }
-
-        $parameters = $this->parameterContainer->getNamedArray();
-        foreach ($parameters as $name => &$value) {
-            if (is_bool($value)) {
-                $type = \PDO::PARAM_BOOL;
-            } elseif (is_int($value)) {
-                $type = \PDO::PARAM_INT;
-            } else {
-                $type = \PDO::PARAM_STR;
-            }
-            if ($this->parameterContainer->offsetHasErrata($name)) {
-                switch ($this->parameterContainer->offsetGetErrata($name)) {
-                    case ParameterContainer::TYPE_INTEGER:
-                        $type = \PDO::PARAM_INT;
-                        break;
-                    case ParameterContainer::TYPE_NULL:
-                        $type = \PDO::PARAM_NULL;
-                        break;
-                    case ParameterContainer::TYPE_LOB:
-                        $type = \PDO::PARAM_LOB;
-                        break;
-                }
-            }
-
-            // parameter is named or positional, value is reference
-            $parameter = is_int($name) ? $name + 1 : $this->driver->formatParameterName($name);
-            $this->resource->bindParam($parameter, $value, $type);
-        }
+        
+        $result = $this->driver->createResult($this->resource, $this);
+        
+        return $result;
     }
 
     /**
